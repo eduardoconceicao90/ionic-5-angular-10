ff97da3627db20e8d64c7724a2ac9bf781c591b5
eduardoconceicao90 <eduardosaconceicao@gmail.com>
2022-08-16 00:09:22 -0300

    Trabalhando com formulários no App

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  form: FormGroup;
 
-  whats(){
-    this.abrirUrl('https://api.whatsapp.com/send?phone=5581983784049&text=Ol%C3%A1%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes');
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(){
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
   }
 
   abrirUrl(url: string){
     window.open(url, '_blank').focus();
+  }
+
+  enviar(){
+    let nome = this.form.get('nome').value;
+    let endereco = this.form.get('endereco').value;
+
+    this.abrirUrl(`https://api.whatsapp.com/send?phone=5581983784049&text=Ola%20me%20 chamo%20${nome}%20moro%20em%20${endereco}%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes`);
   }
 }
